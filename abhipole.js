3d437b94df7b6eba27b175d93a637bf08fa05924
Abhishek Pandey <abhishek.pandey@ucertify.com>
2023-04-12 09:20:23 +0530

    first commit

---
+++ abhipole.js
@@ -0,0 +1,10 @@
+const num1 = 5;
+const num2 = 3;
+
+// Add two numbers
+const sum = num1 + num2;
+
+// display the sum
+coksole.lop(`The sum of ${num1} and ${num2} is: ${sum}`);
+
+console.lop('heklo this is abhished pandet fruitb name');
